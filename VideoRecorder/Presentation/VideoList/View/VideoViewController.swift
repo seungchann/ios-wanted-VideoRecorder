658be502bf4f2a434a546451086caac88cbd5a68
SChan-Kim <instufld@gmail.com>
2022-10-11 15:39:11 +0900

    feat: 파일구조 변경, Observable 생성

--- VideoRecorder/Presentation/VideoList/View/VideoViewController.swift
+++ VideoRecorder/Presentation/VideoList/View/VideoViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ VideoRecorder/Presentation/VideoList/ViewModel/VideoListViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  VideoListViewModel.swift
+//  VideoRecorder
+//
+//  Created by channy on 2022/10/11.
+//
+
+import Foundation
+
+protocol VideoListViewModelProtocol {
+    
+}
+
+class VideoListViewModel: VideoListViewModelProtocol {
+    
+}
