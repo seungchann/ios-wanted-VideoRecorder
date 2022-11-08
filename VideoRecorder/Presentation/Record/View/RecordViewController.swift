54ac0cc5dc672bc4bc551f467cdaf70c02a0a857
shadow9503 <33388081+shadow9503@users.noreply.github.com>
2022-11-08 15:44:43 +0900

    refactor: Refactoring 비디오 권한 요청 및 확인 코드
    - RecordVC -> RecordViewModel
      - 뷰모델의 함수로 이동시킴
      - checkAuthorization(..)

--- VideoRecorder/Presentation/Record/View/RecordViewController.swift
+++ VideoRecorder/Presentation/Record/View/RecordViewController.swift
@@ -113,22 +113,9 @@ class RecordViewController: UIViewController {
         setupConstraints()
         configureView()
         
-        switch AVCaptureDevice.authorizationStatus(for: .video) {
-        case .authorized: // The user has previously granted access to the camera.
-            self.viewModel.setupSession()
-        case .notDetermined: // The user has not yet been asked for camera access.
-            AVCaptureDevice.requestAccess(for: .video) { granted in
-                if granted {
-                    self.viewModel.setupSession()
-                }
-            }
-
-        case .denied: // The user has previously denied access.
-            return
-
-        case .restricted: // The user can't grant access due to restrictions.
-            return
-        }
+//        viewModel.checkAuthorization { isAuth in
+//            self.viewModel.setupSession()
+//        }
     }
     
     deinit {
