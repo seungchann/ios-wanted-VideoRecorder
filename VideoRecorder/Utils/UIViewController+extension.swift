bab8756e8c2d264daf94875eb37daf85b5a70f33
shadow9503 <33388081+shadow9503@users.noreply.github.com>
2022-11-22 19:43:54 +0900

    fix: 수정 alertController dismiss시 처리
    - alert 팝업시 계속해서 메모리가 누적되는 경향이 보임 dismiss시점에서 alertController = nil 처리

--- VideoRecorder/Utils/UIViewController+extension.swift
+++ VideoRecorder/Utils/UIViewController+extension.swift
@@ -9,33 +9,37 @@ import UIKit
 
 extension UIViewController {
 
-    func askForTextAndConfirmWithAlert(title: String, placeholder: String, okHandler: @escaping (String?)->Void) {
+    func askForTextAndConfirmWithAlert(title: String, placeholder: String, okHandler: @escaping (String?) -> Void) {
+        var alertController: UIAlertController?
+        alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
         
-        let alertController = UIAlertController(title: title, message: nil, preferredStyle: .alert)
-        
-        alertController.addTextField { textField in
+        alertController!.addTextField { textField in
             textField.accessibilityIdentifier = "filename"
             textField.placeholder = placeholder
             textField.clearButtonMode = .whileEditing
             textField.borderStyle = .none
-            textField.addTarget(alertController, action: #selector(alertController.textDidChanged), for: .editingChanged)
+            textField.addTarget(alertController!, action: #selector(alertController!.textDidChanged), for: .editingChanged)
         }
-
+        
         let okAction = UIAlertAction(title: "확인", style: .default, handler: { _ in
-            guard let text = alertController.textFields?.first?.text else {
+            guard let text = alertController!.textFields?.first?.text else {
                 return
             }
             okHandler(text)
+            alertController = nil
         })
+        
         okAction.accessibilityIdentifier = "filename_ok"
         okAction.isEnabled = false
-        alertController.addAction(okAction)
+        alertController!.addAction(okAction)
 
-        alertController.addAction(UIAlertAction(title: "취소", style: .cancel, handler: { _ in
+        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: { _ in
             okHandler(nil)
-        }))
-
-        present(alertController, animated: true, completion: nil)
+            alertController = nil
+        })
+        alertController!.addAction(cancelAction)
+        
+        present(alertController!, animated: true, completion: nil)
     }
 }
 
